ede79a94e2f6241d99addcb7422716ed192465e2
MarconiCop <marconifrancisco@live.com>
2021-09-27 19:50:01 -0300

    Modificação feita

--- Projeto_Dimmer_RGB/Projeto_Dimmer_RGB.ino
+++ Projeto_Dimmer_RGB/Projeto_Dimmer_RGB.ino
@@ -1,26 +1,18 @@
-/*******************************************************************************
-* RoboCore Kit Iniciante V8 - 16. - Pisca-Pisca 2
-* Usar um potenciômetro para controlar a intensidade de um LED.
-*******************************************************************************/
 
-const int pinoPotenciometro = A0; // pino onde o pontenciômetro está conectado
-const int pinoLED = 11; // pino onde o LED está conectado
-int leitura = 0; // variável para armazenar o valor lido pelo ADC
-int pwm = 0; // variável para armazenar o valor da razão cíclica (duty cycle) 
+const int pinoPotenciometroVerde = A0; //Controle de verde
+const int pinoLEDGREEN = 10; // LED VERDE
+
+int leituraVerde = 0; //Leitura Verde
+int pwmVerde = 0; // Armazenamento Verde
 
 void setup() {
-  pinMode(pinoPotenciometro, INPUT); // configura o pino com potenciômetro como entrada
-  pinMode(pinoLED, OUTPUT); // configura o pino com o LED como saída
+  pinMode(pinoPotenciometroVerde, INPUT); //Controle de verde
+ 
 }
 
 void loop() {
-  // le o valor de tensão no pino do potenciômetro
-  leitura = analogRead(pinoPotenciometro);
-  
-  // converte o valor do potenciômetro, de 0 a 1023, em um valor de 0 a 255
-  pwm = map(leitura, 0, 1023, 0, 255); 
-  
-  // atualiza a razão cíclica do pino 11, variando a intensidade do LED
-  analogWrite(pinoLED, pwm);
 
+  leituraVerde = analogRead(pinoPotenciometroVerde);
+  pwmVerde = map(leituraVerde, 0, 1023, 0, 255); 
+  analogWrite(pinoLEDGREEN, pwmVerde);
 }
